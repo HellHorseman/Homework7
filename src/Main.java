fbe84a3f74deb1a492477c1df8a3529ef734833c
HellHorseman <duke.sheffer@yandex.ru>
2023-02-24 19:22:03 +0400

    New branch + add task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+    int total = 0;
+    int putAside = 15_000;
+    int month = 1;
+    while (total < 2_459_000) {
+        System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+        month++;
+        total = total + putAside;
+    }
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+
+    }
+    public static void task8 () {
+        System.out.println("Задача 8");
+
     }
 }
